bc9285c1470958cb13cb2f8a49bb0948e3c405dc
Renan Barros <renanbarrosrbs@gmail.com>
2017-12-02 04:52:31 -0200

    recuperando localização

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,21 +23,15 @@ export class HomePage {
   loadMap() {
 
     let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: 43.0741904,
-          lng: -89.3809802
-        },
-        zoom: 18,
-        tilt: 30
+      controls: {
+        'myLocationButton': true
       }
     };
 
     
     this.map = this.googleMaps.create("map_canvas", mapOptions);
-    console.log(this.map);
+    this.map.setMyLocationEnabled(true);
     
-
     // Wait the MAP_READY before using any methods.
     this.map.one(GoogleMapsEvent.MAP_READY)
       .then(() => {
