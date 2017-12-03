e8f321612fdd64279f45ee78ded79670515c6179
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-12-03 12:54:33 -0200

    API Facebook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,22 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import {ApiService} from "../services/apiServices";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -27,6 +30,7 @@ import { LoginPage } from '../pages/login/login';
   providers: [
     StatusBar,
     SplashScreen,
+    ApiService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { HomePage } from '../home/home';
 import { Component } from '@angular/core';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 import { NavController, NavParams } from 'ionic-angular';
+import {ApiService} from "../../services/apiServices";
 
 @Component({
   selector: 'page-login',
@@ -13,9 +14,9 @@ export class LoginPage {
   username: string;
   password: string;
 
-  userData = []
+  userData;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: Facebook) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: Facebook, private apiService: ApiService) {
   }
 
   ionViewDidLoad() {
@@ -24,20 +25,22 @@ export class LoginPage {
 
 
   fbLogin() {
-    console.log('fbLogin()')
     this.fb.login(['public_profile', 'email'])
       .then((res: FacebookLoginResponse) => {
         console.log('Logged into Facebook', res);
         // this.return = JSON.stringify(res);
         this.fb.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', [])
           .then(profile => {
-            this.userData = [
-              { "name": profile.name },
-              { "email": profile.email },
-              { "picture": profile.picture_large.data.url },
-              { "facebook_auth": res.authResponse.accessToken }];
+            this.userData = {
+                name: profile.name,
+                email: profile.email,
+                picture: profile.picture_large.data.url,
+                facebook_auth: res.authResponse.accessToken
+            };
 
-              this.navCtrl.push('EscolhaDeficienciasPage')
+            this.apiService.authFacebook(this.userData).then(user => {
+              this.navCtrl.push('EscolhaDeficienciasPage');
+            });
           })
 
       })
